afa217c3495a3c854514c13423c68616d4c8e1ea
van.___.dev <van973vv@gmail.com>
2024-07-17 18:50:40 +0800

    dssadaa

--- middleware.ts
+++ middleware.ts
@@ -15,11 +15,14 @@ const publicRoutes = ["/auth/login", "/auth/signup", "/"];
 
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
+  console.log(path);
+
   const isProtectedRoute = protectedRoutes.includes(path);
   const isPublicRoute = publicRoutes.includes(path);
 
   const cookie = cookies().get("session")?.value;
   const session = await decrypt(cookie);
+  console.log(session);
   if (isProtectedRoute && !session?.userId) {
     return NextResponse.redirect(new URL("/auth/login", req.nextUrl));
   }
